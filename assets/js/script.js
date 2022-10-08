194c5143a7761e22e043a9c5e01a6254e3763f4f
luizborges146 <113717656+luizborges146@users.noreply.github.com>
2022-10-08 16:26:49 -0700

    Manged to add the api and connect it in JAVA getting a 200 response, however, the 5 days api does not return the city name, so looking a way to change it it my code

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,10 @@ let weather={
     apiKey:"18f2b4783decaf3b750cf7554dde6fa5",
     fetchWeather:function(city){
         fetch(
+            // "https://api.openweathermap.org/data/2.5/weather?q="
+            // + city 
+            // + "&units=metric&appid=" 
+            // + this.apiKey
             "https://api.openweathermap.org/data/2.5/forecast?q="
             + city
             + "&units=metric&appid="
@@ -11,9 +15,20 @@ let weather={
         .then((data) => this.displayWeather(data));
     },
     displayWeather:function(data) {
-        var {name} = data;
+        //var {name} = data;
         var {icon,description} = data.weather[0];
-        var {tem,humidity} = data.main;
+        var {temp,humidity} = data.main;
         var {speed} = data.wind;
+        console.log("City name: " + this.name
+                    + " \nsymbol :" + icon 
+                    + " \nWeather Desc : " + description
+                    + " \nTemperature " + temp
+                    + " \nHumidity: " + humidity 
+                    + " \nWind speed: " + speed);
+
+        // var city = document.querySelectorAll(".city")//.innerText = "Weather today in " + name;
+        // for(var i = 0; i < city.length; i++){
+        //     city.innerText = "Weather today in " + name;
+        // };
     }
 }
