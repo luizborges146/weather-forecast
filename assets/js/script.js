ed7209b66828ff3e2fcfab3ffd641162de3c085d
luizborges146 <113717656+luizborges146@users.noreply.github.com>
2022-10-08 17:30:33 -0700

    managed to retrive data from the API response and add it to a variable and print it out

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,20 +15,22 @@ let weather={
         .then((data) => this.displayWeather(data));
     },
     displayWeather:function(data) {
-        //var {name} = data;
-        var {icon,description} = data.weather[0];
-        var {temp,humidity} = data.main;
-        var {speed} = data.wind;
-        console.log("City name: " + this.name
+        console.log(data);
+        var {name} = data.city.name;
+        var {icon,description} = data.list[0].weather[0];
+        var {temp,humidity} = data.list[0].main;
+        var {speed} = data.list[0].wind;
+        console.log("City name: " + name
                     + " \nsymbol :" + icon 
                     + " \nWeather Desc : " + description
                     + " \nTemperature " + temp
                     + " \nHumidity: " + humidity 
-                    + " \nWind speed: " + speed);
+                    + " \nWind speed: " + speed
+                    );
 
-        // var city = document.querySelectorAll(".city")//.innerText = "Weather today in " + name;
-        // for(var i = 0; i < city.length; i++){
-        //     city.innerText = "Weather today in " + name;
-        // };
+        var city = document.querySelectorAll(".city")//.innerText = "Weather today in " + name;
+        for(var i = 0; i < city.length; i++){
+            city.innerText = "Weather today in " + name;
+        };
     }
 }
