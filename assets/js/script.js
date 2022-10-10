b6c9f2459a30ac9dfa5379d1634ebefaa397d521
luizborges146 <113717656+luizborges146@users.noreply.github.com>
2022-10-09 22:43:46 -0700

    Created another Fetch for the 5 days forecast, I can print it out but at the moment, facing an issue to link them with the HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
+var apiKey="18f2b4783decaf3b750cf7554dde6fa5";
 var weather={
-    apiKey:"18f2b4783decaf3b750cf7554dde6fa5",
+    /* ##################### Current Weather ###################################*/
     fetchWeather:function(city){
         fetch(
             "https://api.openweathermap.org/data/2.5/weather?q="
             + city 
             + "&units=metric&appid=" 
-            + this.apiKey
+            + apiKey
             // "https://api.openweathermap.org/data/2.5/forecast?q="
             // + city
             // + "&units=metric&appid="
@@ -40,9 +41,43 @@ var weather={
         //     city.innerText = "Weather today in " + name;
         // };
     },
+    /* ##################### Weekly Weather ###################################*/
+    fetchWeatherW:function(city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/forecast?q="
+            + city 
+            + "&units=metric&appid=" 
+            + apiKey
+        )
+        .then((response) => response.json())
+        .then((data) => this.displayWeatherW(data));
+
+    },
+    displayWeatherW:function(data) {
+        console.log(data);
+        var name = data.city.name;
+        // for (var i = 0; i < 1; i++){
+            var {icon, description} = data.list[0].weather[0];
+            var {temp, humidity} = data.list[0].main;
+            var { speed } = data.list[0].wind;
+            console.log(name,icon,description,temp,humidity,speed + " Test if this is going to be displayed");
+
+        // document.querySelector(".city").innerText = "Weather in " + name;
+        // document.querySelector("#icon0").src = "https://openweathermap.org/img/wn/" + icon +".png";
+        //document.querySelector(".description0").innerText = description;
+        document.querySelector(".temp0").innerText = temp + "°C";
+        // document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        // document.querySelector(".wind").innerText = "Wind Speed: " + speed + "Km/h";
+        // }
+    },
+
+    /* ##################### End Weekly Weather ###################################*/
     search:function(){
         this.fetchWeather(document.querySelector(".search-bar").value)
+        this.fetchweatherW(document.querySelector(".search-bar").value)
     }
+    
+    
 };
 document.querySelector(".search button").addEventListener("click", function(){
     weather.search();
@@ -53,4 +88,60 @@ document.querySelector(".search-bar").addEventListener("keyup",function(event){
     }
 })
 weather.fetchWeather("London");
-/*-------------------------------------- Current weather info finish here -----------------------------------------*/
+weather.fetchWeatherW("London");
+
+
+/*-------------------------------------- WEEKLY weather info-----------------------------------------*/
+
+// var weatherWeekly={
+//     apiKey:"18f2b4783decaf3b750cf7554dde6fa5",
+//     fetchWeatherWeek:function(city){
+//         fetch(
+//             "https://api.openweathermap.org/data/2.5/forecast?q="
+//             + city
+//             + "&units=metric&appid="
+//             + this.apiKey
+//         )
+//         .then((response) => response.json())
+//         .then((data) => this.displayWeather(data));
+//     },
+//     displayWeather:function(data) {
+//         console.log(data);
+//         // for(var i = 0; i > data.length;i++){
+//         //     console.log(data[i]);
+//         //     i++;
+
+//             // var {name} = data;// ask the support assistance tomorrow
+//             // var {country} = data.sys;
+//             // var {icon,description} = data.weather[0];
+//             // var {temp,humidity} = data.main;
+//             // var {speed} = data.wind;
+//             // console.log("City name: " + name
+//             //             + " \nsymbol :" + icon 
+//             //             + " \nWeather Desc : " + description
+//             //             + " \nTemperature " + temp
+//             //             + " \nHumidity: " + humidity 
+//             //             + " \nWind speed: " + speed
+//             //             // + " \nDate: " + dt_text
+//             //             );
+//             // document.querySelector(".city").innerText = "Weather today in " + name + " / " + country;
+//             // document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon +".png";
+//             // document.querySelector(".description").innerText = description;
+//             // document.querySelector(".temp").innerText = temp + "°C";
+//             // document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+//             // document.querySelector(".wind").innerText = "Wind Speed: " + speed + "Km/h";
+
+//         },
+//     search:function(){
+//         this.fetchWeather(document.querySelector(".search-bar").value)
+//     }
+// };
+// // document.querySelector(".search button").addEventListener("click", function(){
+// //     weather.search();
+// // });
+// // document.querySelector(".search-bar").addEventListener("keyup",function(event){
+// //     if(event.key == "Enter") {
+// //         weather.search();
+// //     }
+// // })
+// // weather.fetchWeather("London");
