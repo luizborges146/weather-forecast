661fa97c45264eb86a325e8f121951e53631ba4e
luizborges146 <113717656+luizborges146@users.noreply.github.com>
2022-10-09 18:22:57 -0700

    I need to correct the whole JavaScript, as I was taking the 5 day forecast as the current weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,36 @@
-let weather={
+var weather={
     apiKey:"18f2b4783decaf3b750cf7554dde6fa5",
     fetchWeather:function(city){
         fetch(
-            // "https://api.openweathermap.org/data/2.5/weather?q="
-            // + city 
-            // + "&units=metric&appid=" 
-            // + this.apiKey
-            "https://api.openweathermap.org/data/2.5/forecast?q="
-            + city
-            + "&units=metric&appid="
+            "https://api.openweathermap.org/data/2.5/weather?q="
+            + city 
+            + "&units=metric&appid=" 
             + this.apiKey
+            // "https://api.openweathermap.org/data/2.5/forecast?q="
+            // + city
+            // + "&units=metric&appid="
+            // + this.apiKey
         )
         .then((response) => response.json())
         .then((data) => this.displayWeather(data));
     },
     displayWeather:function(data) {
         console.log(data);
-        var name = data.city.name;// ask the support assistance tomorrow
-        var nameCountry = data.city.country;
-        var {icon,description} = data.list[0].weather[0];
-        var {temp,humidity} = data.list[0].main;
-        var {speed} = data.list[0].wind;
+        var {name} = data;// ask the support assistance tomorrow
+        var {country} = data.sys;
+        var {icon,description} = data.weather[0];
+        var {temp,humidity} = data.main;
+        var {speed} = data.wind;
+        // var {dt_text} = data.list[0]dt_text;
         console.log("City name: " + name
                     + " \nsymbol :" + icon 
                     + " \nWeather Desc : " + description
                     + " \nTemperature " + temp
                     + " \nHumidity: " + humidity 
                     + " \nWind speed: " + speed
+                    // + " \nDate: " + dt_text
                     );
-        document.querySelector(".city").innerText = "Weather today in " + name + " / " + nameCountry;
+        document.querySelector(".city").innerText = "Weather today in " + name + " / " + country;
         document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon +".png";
         document.querySelector(".description").innerText = description;
         document.querySelector(".temp").innerText = temp + "°C";
@@ -42,13 +44,13 @@ let weather={
         this.fetchWeather(document.querySelector(".search-bar").value)
     }
 };
-
 document.querySelector(".search button").addEventListener("click", function(){
     weather.search();
 });
-
 document.querySelector(".search-bar").addEventListener("keyup",function(event){
     if(event.key == "Enter") {
         weather.search();
     }
 })
+weather.fetchWeather("London");
+/*-------------------------------------- Current weather info finish here -----------------------------------------*/
